65a122a7020fcb1bff1458a278d4e538228b2840
ALLAN <allankipr6@gmail.com>
2024-03-26 12:51:21 +0300

    new

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+function findMatching(collection,string){
+    return collection.filter(
+        (tt) => tt.toLowerCase() === string.toLowerCase());
+    }
+function fuzzyMatch(collection,string){
+    return collection.filter(
+        tt => tt.toLowerCase().indexOf(string.toLowerCase())===0
+    );
+}
+function matchName(collection,string){
+    return collection.filter(
+        (tt) => tt.name === string
+    );
+}
